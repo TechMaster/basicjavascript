2fef16e0140047793c8e0715ec7332ec8bd6805e
TechMaster <cuong@techmaster.vn>
2015-05-15 18:24:56 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by cuong on 15/05/2015.
+ */
+"use strict";
+console.log("Hello from Web Storm");
+console.log(typeof "Hello World");
+console.log(typeof 3.14);
+console.log(typeof true);
+var x = 10;
+console.log(typeof x);
+x = 11;
+console.log("x = " + x);
+let y = 12;
+console.log(typeof y);
+y = 13;
+const z = 30;
+//z = 31;
